d0c2e72a04ba3ede9319d5432644e8a712f41086
Esteban Borai <estebanborai@outlook.com>
2019-07-24 21:07:04 -0300

    Add missing CRUD operations

--- lib/controller/todo_controller.dart
+++ lib/controller/todo_controller.dart
@@ -42,4 +42,27 @@ class TodoController extends ResourceController {
 
 			return Response.ok(createdTodo);
 	}
+
+	@Operation.put('id')
+	Future<Response> updateTodo(@Bind.path('id') int id) async {
+		final Map<String, dynamic> body = await request.body.decode();
+    final query = Query<Todo>(context)
+      ..values.title = body['title'] as String
+      ..values.description = body['description'] as String
+      ..values.status = body['status'] as String
+      ..where((t) => t.id).equalTo(id);
+
+    final updatedTodo = await query.updateOne();
+    return Response.ok(updatedTodo);
+	}
+
+  @Operation.delete('id')
+  Future<Response> deleteTodo(@Bind.path('id') int id) async {
+    final query = Query<Todo>(context)
+      ..where((t) => t.id).equalTo(id);
+
+    final int todosDeleted = await query.delete();
+
+    return Response.ok(todosDeleted);
+  }
 }
